f52772d0dd2a9e6ed775fc489e68159b2365f85e
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2021-08-22 22:16:59 +0200

    add seasonality based on Open Close

--- stooq.py
+++ stooq.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import mplfinance as mpl
-import statsmodels as st
+import time
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 # Download daily historical data for DAX
 ticker = '^dax'
@@ -16,9 +17,27 @@ urlretrieve(url, csv_file)
 # Read the data
 data = pd.read_csv(csv_file, index_col='Date', parse_dates=['Date'], date_parser=lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
-data.tail()
+data.dropna()
+
+data = data.tail(1000)
 
 #mpl.plot(data.tail(100), type='candle', volume=False)
 
 # Descriptive statistics
 print(data.describe())
+
+data['OC'] = data['Close']-data['Open']
+
+print(data.describe())
+
+data['OCa'] = data['OC'] - data['OC'].min() + 1
+analysis = data[['OCa']].copy()
+
+decompose_result_mult = seasonal_decompose(analysis, model="multiplicative", period=365)
+
+trend = decompose_result_mult.trend
+seasonal = decompose_result_mult.seasonal
+residual = decompose_result_mult.resid
+
+fig = decompose_result_mult.plot()
+fig.savefig('result.png')
